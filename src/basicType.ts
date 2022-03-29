d91a721cb1184530c078d7ce817c4553abea79f7
seolhyelin <tosiltosilkk@gmail.com>
2022-03-29 14:26:26 +0900

    Add: typescript 기본타입

---
+++ src/basicType.ts
@@ -0,0 +1,20 @@
+let numValue: number;
+let stringValue: string;
+let booleanValue: boolean;
+let undefinedValue: undefined;
+let nullValue: null;
+let objValue: object;
+let symbolValue: symbol;
+
+let nameList: string[];
+nameList = ["1", "3"];
+nameList.push("44");
+
+let user1: { name: string; score: number };
+user1 = {
+  name: "jay",
+  score: 30,
+};
+
+let tuple2: [number, String];
+tuple2 = [1, "hello"];
